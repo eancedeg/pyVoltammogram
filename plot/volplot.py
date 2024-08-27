f42d383b70d49a3a9dc48df8bbf1f675f753a93f
Evys Ancede Gallardo <eancedeg@gmail.com>
2024-08-27 05:45:22 -0400

    Initial commit

---
+++ plot/volplot.py
@@ -0,0 +1,15 @@
+from matplotlib.figure import Figure
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+plt.style.use('ggplot')
+
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None):
+        figure = Figure()
+        super().__init__(figure)
+        self.setParent(parent)
+
+        self.axes = self.figure.add_subplot(111)
+        self.axes.set_xlabel('V (mV)')
+        self.axes.set_ylabel('i (mA)')
---
+++ pyVoltammetry.py
@@ -0,0 +1,80 @@
+import sys
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QFileDialog, QVBoxLayout, QListWidgetItem
+from ui.VoltamUI import Ui_VoltammetryWindow
+from plot.volplot import MplCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+
+class VoltWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+        self.ui = Ui_VoltammetryWindow()
+        self.ui.setupUi(self)
+
+        # Matplotlib
+        self.matplotlib_widget = MplCanvas(self.ui.plotwidget)
+        self.toolbar = NavigationToolbar(self.matplotlib_widget, self)
+
+        layout = QVBoxLayout(self.ui.plotwidget)
+        layout.addWidget(self.toolbar)
+        layout.addWidget(self.matplotlib_widget)
+
+        # Events
+        self.ui.actionOpenFile.triggered.connect(self.openfile)
+        self.ui.cycles.itemClicked.connect(self.update_plot)
+
+    def openfile(self):
+        file_name = QFileDialog.getOpenFileName(self, 'Select File ...', '',
+                                                'Text Files (*.txt);;All Files (*)')
+        if file_name[0]:
+            with open(file_name[0]) as data_file:
+                section = 1
+                x_data = []
+                y_data = []
+                for line in data_file:
+                    line_data = line.strip()
+                    if line_data:
+                        try:
+                            x, y = [float(n) for n in line_data.split(',')]
+                            x_data.append(x)
+                            y_data.append(y)
+                        except ValueError as e:
+                            print(f'Error processing line: {line_data}, Error: {e}')
+                    else:
+                        if x_data and y_data:
+                            # print(f'Data before clearing: X -> {x_data}, Y -> {y_data}')
+                            self.add_cycle(f'Cycle {section}', x_data, y_data)
+                            section += 1
+                            x_data = []
+                            y_data = []
+                if x_data and y_data:
+                    # print(f'Data before clearing: X -> {x_data}, Y -> {y_data}')
+                    self.add_cycle(f'Cycle {section}', x_data, y_data)
+
+    def update_plot(self, item):
+        mydata = item.data(100)
+        if mydata:
+            x_data, y_data = mydata
+            self.matplotlib_widget.axes.clear()
+
+            self.matplotlib_widget.figure.subplots_adjust(right=0.99, left=0.15)
+
+            self.matplotlib_widget.axes.plot(x_data, y_data)
+            self.matplotlib_widget.axes.set_xlabel('V (mV)')
+            self.matplotlib_widget.axes.set_ylabel('i (mA)')
+            self.matplotlib_widget.axes.set_title('Cyclic Voltammogram')
+            self.matplotlib_widget.draw()
+
+    def add_cycle(self, name, x_data, y_data):
+        # print(f'Storing data: {name}, X -> {x_data}, Y -> {y_data}')
+        item = QListWidgetItem(name)
+        item.setData(100, (x_data, y_data))
+        self.ui.cycles.addItem(item)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = VoltWindow(None)
+    window.show()
+    sys.exit(app.exec_())
