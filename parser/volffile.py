3c699038aa0bdb9e708b840fefca194ac2f83a85
Evys Ancede Gallardo <eancedeg@gmail.com>
2024-09-10 00:00:56 -0300

    Parser code

---
+++ parser/volffile.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+
+class Voltammogram(object):
+    def __init__(self, file):
+        self.file = file
+        self.date = ''
+        self.technique = ''
+        self.voltdata = []
+
+        line = 'Data'
+        data_section = False
+        header = True
+        with open(self.file) as volt:
+            while line:
+                if header:
+                    # Date
+                    line = volt.readline().strip()
+                    self.date = datetime.strptime(line, '%b. %d, %Y   %H:%M:%S')
+
+                    # Technique
+                    self.technique = volt.readline().strip()
+
+                    for i in range(6):
+                        volt.readline()
+
+                    # Voltammogram parameters
+                    self.initvolt = float(volt.readline().split('=')[1])
+                    self.highvolt = float(volt.readline().split('=')[1])
+                    self.lowvolt = float(volt.readline().split('=')[1])
+                    self.scandir = volt.readline().split('=')[1].strip()
+                    self.scanrate = float(volt.readline().split('=')[1])
+                    self.segments = int(volt.readline().split('=')[1])
+                    self.sample_interval = float(volt.readline().split('=')[1])
+
+                    while not data_section:
+                        line = volt.readline()
+                        if 'Potential/V, Current/A' in line:
+                            line = volt.readline()
+                            header = False
+                            data_section = True
+                if data_section:
+                    line = volt.readline()
+                    if line:
+                        print(f'Algo -> {line}')
+
+                        print([(float(v.strip()), float(i.strip())) for v, i in [line.split(',')]])
+
+
+if __name__ == '__main__':
+    v = Voltammogram('../src/cv sb-5(-1800a1400) 200 mvs.txt')
+    print(v.voltdata)
